e6ca117a351bd8073b5ec0ed2ef0a962a3d0f40d
luiizr <luizroberto312005@gmail.com>
2025-09-16 12:13:43 -0300

    Ajustando arquitetura

--- FitHub/apps/backend/src/main.ts
+++ FitHub/apps/backend/src/main.ts
@@ -1,11 +1,11 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
+import pool from './app/database';
 import { Logger } from '@nestjs/common';
+import * as dotenv from 'dotenv';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+
+// Responsável por carregar as variáveis do .env para o database
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
