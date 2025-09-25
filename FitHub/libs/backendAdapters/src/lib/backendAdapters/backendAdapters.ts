ac0377d1ab8d0e5461f0b7b1d092f7f6d022cc31
luiizr <luizroberto312005@gmail.com>
2025-09-25 10:17:13 -0300

    Feat: Implementa pastas de arquitetura

---
+++ FitHub/libs/backendAdapters/src/lib/backendAdapters/backendAdapters.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-backend-adapters',
+  imports: [],
+  templateUrl: './backendAdapters.html',
+  styleUrl: './backendAdapters.css',
+})
+export class BackendAdapters {}
---
+++ FitHub/libs/shareds/README.md
@@ -0,0 +1,7 @@
+# shareds
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test shareds` to execute the unit tests.
